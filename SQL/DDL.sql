6350cd05c4c0af7b889933d946bca2115ac4fd18
Faris Mohamed <farismohamedCS@gmail.com>
2020-04-11 15:59:54 -0400

    Added more tables in the database

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -5,15 +5,41 @@ CREATE TABLE author (
 );
 
 CREATE TABLE book (
-  bookID serial primary key, /* Reasoning behind this is we can have multiple of the same book in the database*/
-  bookASIN char(10), /* ASIN are garunteed to be size 10*/
+  bookASIN char(10) primary key, /* ASIN are garunteed to be size 10*/
+  sellerID text primary key, 
   bookTitle text,
   bookGenre text,
   bookPages integer,
   bookPrice numeric(6, 2),
   percentageTaken numeric(3,2), /* Todo limit of 100 percent*/
-  orderId integer /* Show non-sold books by filtering for nulls, sold-books can be queried by their orderID*/
+  inventory integer -- # left in stock
 );
+
+CREATE TABLE ORDER( /* Store multiple of these for each book in the order. Primary Key (ORDERID,BOOKASIN)*/ 
+    bookASIN char(10) primary key,
+    quantity integer,
+    orderID text primary key,
+    sellerID text,
+    userID text,
+);
+
+CREATE TABLE SHIPMENT(
+    orderID text primary key references ORDER(orderID) on delete cascade,
+    trackingID text primary key
+);
+
+
+CREATE TABLE PAYMENT_INFO(
+    creditCardNumber
+);
+
+
+
+
+
+
+
+
 
 
 CREATE TABLE author_book ( /* Allows for many to many relation between authors and books*/
@@ -23,17 +49,18 @@ CREATE TABLE author_book ( /* Allows for many to many relation between authors a
 );
 
 CREATE TABLE user( /* needs work for later*/
-    userID serial primary key,
+    userID text primary key, /* Created by application side*/
+    userEmail text,
+    avatarURL text,
     fullName text
 );
 
-CREATE TABLE ORDER( /* TODO: Figure out logic behind this*/ 
-    userID references user(userID),
-    sellerID refrences user(userID),
-    orderID serial primary key,
-    trackingURL text,
 
-);
+CREATE TABLE SELLER(
+
+  userID serial primary key,
+
+)
 
 
 
