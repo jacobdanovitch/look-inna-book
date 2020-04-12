e8616130d2df261e30a06e9046accda770a52b40
Faris Mohamed <farismohamedCS@gmail.com>
2020-04-11 21:55:39 -0400

    Updated schema (rough copy) of completed schema

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -4,47 +4,31 @@ CREATE TABLE author (
   unique(authorName) /* Authours should be unique, we should not have duplicate authors*/
 );
 
-CREATE TABLE book (
-  bookASIN char(10) primary key, /* ASIN are garunteed to be size 10*/
-  sellerID text primary key, 
+CREATE TABLE book ( /* Primary key (bookASIN,sellerID) */
+  bookASIN char(10), /* ASIN are garunteed to be size 10*/
+  sellerID text, 
   bookTitle text,
+  bookPublisher text,
   bookGenre text,
   bookPages integer,
   bookPrice numeric(6, 2),
   percentageTaken numeric(3,2), /* Todo limit of 100 percent*/
   inventory integer -- # left in stock
+  book_seller_pkey primary key(bookASIN, sellerID)
 );
-
-CREATE TABLE ORDER( /* Store multiple of these for each book in the order. Primary Key (ORDERID,BOOKASIN)*/ 
-    bookASIN char(10) primary key,
-    quantity integer,
-    orderID text primary key,
-    userID text,
-);
-
-CREATE TABLE SHIPMENT(
-    orderID text primary key references ORDER(orderID) on delete cascade,
-    trackingID text primary key
-);
-
-
-CREATE TABLE PAYMENT_INFO( /* TODO: IMPLEMENT*/
-    creditCardNumber
-);
-
-
-
-
-
-
-
-
-
 
 CREATE TABLE author_book ( /* Allows for many to many relation between authors and books*/
   authorID integer references author(authorID) on delete cascade,
   bookID   integer references book(bookID) on delete cascade,
   constraint author_book_pkey primary key(authorID, bookID)
+);
+
+
+CREATE TABLE seller( /* needs work for later*/
+    sellerID text primary key, /* Created by application side*/
+    userEmail text,
+    avatarURL text,
+    fullName text
 );
 
 CREATE TABLE user( /* needs work for later*/
@@ -55,11 +39,53 @@ CREATE TABLE user( /* needs work for later*/
 );
 
 
-CREATE TABLE SELLER(
 
-  userID serial primary key,
+CREATE TABLE order( /* Store multiple of these for each book in the order. Primary Key (ORDERID,BOOKASIN)*/ 
+    bookASIN char(10),
+    quantity integer,
+    orderID text,
+    userID text,
+    sellerID text,
+    book_seller_pkey primary key(bookASIN,sellerID,userID,orderID)
+);
 
-)
+
+
+CREATE TABLE shipment(
+    orderID text primary key references ORDER(orderID) on delete cascade,
+    trackingID text primary key,
+);
 
 
 
+
+CREATE TABLE publisher( /* Needs to be finished*/
+  bankingInformation text primary key,
+  bookPublisher text,
+);
+
+
+CREATE TABLE PhoneNumber(
+  PhoneNumber text primary key,
+  bookPublisher text, /*Whoever owns the phone number*/
+);
+
+
+CREATE TABLE payment_info(
+  creditCardNumber char(16) primary key,
+  cvv char(3),
+  expirationDate char(6), /* maybe look at date object*/
+  ownerUserID text /*Whoever actually owns the card*/
+);
+
+
+CREATE TABLE address(
+  owner_id text primary key /*Publisher/Seller/User ID*/
+  houseNumber int,
+  street text,
+  city text,
+  province text,
+  country text,
+  postal_code text
+);
+  
