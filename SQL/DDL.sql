4e9ff4c5104135d8e1bacc7dc07c10ee2194c64a
Faris Mohamed <farismohamedCS@gmail.com>
2020-04-13 02:34:35 -0400

    Added 99 percent done changes

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,12 +1,25 @@
 CREATE TABLE book ( /* Primary key (bookASIN,sellerID) */
   bookASIN char(10) primary key, /* ASIN are garunteed to be size 10*/
-  authorName text, 
   bookTitle text,
   bookPublisher text,
   bookGenre text,
   bookPages integer,
   bookPrice numeric(6, 2),
   percentageTaken numeric(3,2) /* Todo limit of 100 percent*/
+);
+
+CREATE TABLE author(
+  authourName text primary key 
+);
+
+
+
+CREATE TABLE author_book ( /* Allows for many to many relation between authors and books*/
+  authourName text,
+  bookASIN char(10),
+  primary key(authourName,bookASIN),
+  foreign key (authourName) references author,
+  foreign key (bookASIN) references book
 );
 
 
@@ -18,13 +31,13 @@ CREATE TABLE buyer(
 
 
 
-CREATE TABLE user_order( /* Store multiple of these for each book in the order. Primary Key (ORDERID,BOOKASIN)*/ 
+CREATE TABLE user_order( /* These need to be stored like these in order for the database to be considered normalized... (Ties user to order)*/ 
     orderID text primary key,
     userID text,
     foreign key (userID) references buyer
 );
 
-CREATE TABLE order_book(
+CREATE TABLE order_book( /* These need to be stored like these in order for the database to be considered normalized... (Ties purchased books to order)*/ 
   orderID text,
   bookASIN char(10),
   quantity integer,
@@ -34,19 +47,13 @@ CREATE TABLE order_book(
 
 
 
-CREATE TABLE shipment(
+CREATE TABLE shipment( /* These need to be stored like these in order for the database to be considered normalized... (Ties shipment to order)*/ 
     orderID text,
     trackingID text primary key,
     foreign key (orderID) references user_order
 );
 
 
-
-
-CREATE TABLE publisher( /* Needs to be finished*/
-  bankingInformation text primary key,
-  bookPublisher text
-);
 
 
 CREATE TABLE PhoneNumber(
@@ -62,15 +69,17 @@ CREATE TABLE credit_cards(
 );
 
 
-CREATE TABLE credit_cards_owner(
-  creditCardNumber char(16),
+CREATE TABLE credit_cards_owner( /* Many to many relation between credit cards and owner*/
   ownerUserID text,
-  primary key(creditCardNumber,ownerUserID)
+  creditCardNumber char(16),
+  primary key(creditCardNumber,ownerUserID),
+  foreign key (creditCardNumber) references credit_cards,
+  foreign key (ownerUserID) references buyer
 );
 
 
-CREATE TABLE residence(
-  owner_id text primary key, /*Publisher/Seller/User ID*/
+CREATE TABLE residence( /* can tie to both and normalized properly but idk if jacob is gonna be okay with, bring up tommorow*/
+  house_id text primary key, 
   houseNumber int,
   street text,
   city text,
@@ -78,4 +87,18 @@ CREATE TABLE residence(
   country text,
   postal_code text
 );
-  
+
+CREATE TABLE buyer_residence(
+  house_id text,
+  userID text,
+  primary key (house_id,userID),
+  foreign key (house_id) references residence,
+  foreign key (userID) references buyer
+);
+
+CREATE TABLE publisher( /* Needs to be finished*/
+  bankingInformation text primary key,
+  bookPublisher text,
+  residence_id text,
+  foreign key (residence_id) references residence
+);
